2f4dcfc1b533caf24e709413975fa4408a64c57a
Michail Levykin <michailinlevyk@gmail.com>
2026-01-17 06:05:07 +0500

    Добавил формулу покупки билетов

--- Lesson1.py
+++ Lesson1.py
@@ -2,3 +2,14 @@ msc_price = 13_000
 spb_price = 10_000
 ekb_price = 8_000
 
+dist = input("Введите пункт назначения (msc, spb, ekb): ")
+count = int(input("Введите количество людей: "))
+
+if dist == "msc":
+    price = msc_price * count
+elif dist == "spb":
+    price = spb_price * count
+elif dist == "ekb":
+    price = ekb_price * count
+
+print("Цена поездки: ", price, "руб.")
